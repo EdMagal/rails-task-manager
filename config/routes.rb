3fa152af9c70263869bdf97ae0ed776d4c530589
eduardo pimenta <ed83isme@gmail.com>
2021-10-20 20:46:14 -0300

    my solution to the task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # INDEX
+  get 'tasks', to: 'tasks#list'
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'  
+  # SHOW / FIND
+  get 'tasks/:id', to: 'tasks#find', as: 'task'
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
+  patch 'tasks/:id', to: 'tasks#update'
+  # DESTROY
+  delete 'tasks', to: 'tasks#destroy'
+  # HOMEPAGE
+  root to: 'tasks#list'
 end
