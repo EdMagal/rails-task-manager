30e634586669ad1f0a9b52cb00438e429794eed6
eduardo pimenta <ed83isme@gmail.com>
2021-10-20 21:01:40 -0300

    fixed some issues with the routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # INDEX
-  get 'tasks', to: 'tasks#list'
+  get 'tasks', to: 'tasks#list', as: 'tasks'
   # CREATE
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
   post 'tasks', to: 'tasks#create'  
